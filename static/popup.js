6ffb255d362f12612e64d86852d8b87577741480
Johan Bjäreholt <johan@bjareho.lt>
2020-05-03 21:25:45 +0200

    refactor: Rewrite popup rendering

--- static/popup.js
+++ static/popup.js
@@ -2,29 +2,34 @@
 
 function renderStatus() {
   chrome.storage.local.get(["lastSync", "lastSyncSuccess", "testing", "baseURL", "enabled"], function(obj) {
-    let lastSyncString = obj.lastSync ? new Date(obj.lastSync).toLocaleString() : "never";
-    let runningStatusColor = obj.lastSyncSuccess ? "#00AA00" : "#ff0000";
-    var msg = "<table>";
-    msg += "<tr>" +
-      "<th>Connected:</th>" + "<td style='color: " + runningStatusColor + "; font-size: 1.5em;'>✔</td>" +
-    "</tr>";
-    if(obj.testing == true) {
-      msg += "<tr>" +
-        "<th>Testing:</th>" + "<td style='color: #FF8800; font-size: 1.5em;'>✔</td>" +
-      "</tr>";
+    // Enabled checkbox
+    document.getElementById('status-enabled-checkbox').checked = obj.enabled;
+
+    // Connected
+    let connectedColor = obj.lastSyncSuccess ? "#00AA00" : "#FF0000";
+    let connectedCharacter = obj.lastSyncSuccess ? "✔" : "✖";
+    let element = document.getElementById('status-connected-icon');
+    element.innerHTML = connectedCharacter;
+    element.style = "color: " + connectedColor + ";";
+
+    // Testing
+    if (obj.testing == true) {
+      let element = document.getElementById('testing-notice');
+      element.innerHTML = "Extension is running in testing mode";
+      element.style = "color: #F60; font-size: 1.2em;";
     }
-    msg += "<tr>" +
-      "<th>Last sync:</th>" + "<td>" + lastSyncString + "</td>" +
-    "</tr>";
-    msg += "</table>";
-    document.getElementById('status').innerHTML = msg;
+
+    // Last sync
+    let lastSyncString = obj.lastSync ? new Date(obj.lastSync).toLocaleString() : "never";
+    document.getElementById('status-last-sync').innerHTML = lastSyncString;
+
+    // Set webUI button link
     document.getElementById('webui-link').href = obj.baseURL;
-    document.getElementById('watcher-enabled-checkbox').checked = obj.enabled;
   });
 }
 
 function domListeners() {
-  let enabled_checkbox = document.getElementById('watcher-enabled-checkbox');
+  let enabled_checkbox = document.getElementById('status-enabled-checkbox');
   enabled_checkbox.addEventListener("change", (obj) => {
     let enabled = obj.srcElement.checked;
     chrome.runtime.sendMessage({enabled: enabled}, function(response) {});

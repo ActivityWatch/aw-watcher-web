4e752e25cd75f4c35d988002f64777e6b8d4fb68
Morpheus0x <morpheus0x@pm.me>
2023-01-10 15:09:43 +0100

    renamed refuse button, since an extension can't uninstall itself

--- static/consent.js
+++ static/consent.js
@@ -4,8 +4,7 @@ function consentListeners() {
   let consent_refused = document.getElementById('consent-refused');
   let consent_given = document.getElementById('consent-given');
   consent_refused.addEventListener("click", (obj) => {
-    // Source: https://stackoverflow.com/questions/39154543/can-a-firefox-extension-uninstall-itself
-    // TODO: Uninstall Extension
+    // TODO: close popup
   });
   consent_given.addEventListener("click", (obj) => {
     chrome.runtime.sendMessage({enabled: true}, function(response) {});

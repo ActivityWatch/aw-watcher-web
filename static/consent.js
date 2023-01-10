c3cfd8164742b1c0beea4d25c5d642ced2f22240
Morpheus0x <morpheus0x@pm.me>
2023-01-10 15:13:30 +0100

    implemented button actions

--- static/consent.js
+++ static/consent.js
@@ -4,11 +4,12 @@ function consentListeners() {
   let consent_refused = document.getElementById('consent-refused');
   let consent_given = document.getElementById('consent-given');
   consent_refused.addEventListener("click", (obj) => {
-    // TODO: close popup
+    window.close()
   });
   consent_given.addEventListener("click", (obj) => {
     chrome.runtime.sendMessage({enabled: true}, function(response) {});
-    // TODO: Dismiss Popup
+    chrome.storage.local.set({"noConsentGiven": false});
+    window.close()
   })
 }
 

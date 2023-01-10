fb587aaf994f1ac2c533ab53c8e8ad85b4b2fbf3
Morpheus0x <morpheus0x@pm.me>
2023-01-10 15:11:21 +0100

    implemented extra consent button, if first popup was denied

--- static/popup.js
+++ static/popup.js
@@ -1,9 +1,23 @@
 "use strict";
 
 function renderStatus() {
-  chrome.storage.local.get(["lastSync", "lastSyncSuccess", "testing", "baseURL", "enabled"], function(obj) {
+  chrome.storage.local.get(["lastSync", "lastSyncSuccess", "testing", "baseURL", "enabled"], async function(obj) {
     // Enabled checkbox
-    document.getElementById('status-enabled-checkbox').checked = obj.enabled;
+    let enabledCheckbox = document.getElementById('status-enabled-checkbox');
+    enabledCheckbox.checked = obj.enabled;
+
+    // Consent Button
+    let showConsentBtn = document.getElementById('status-consent-btn');
+    chrome.storage.local.get("noConsentGiven", (obj) => {
+      console.log('noConsentGiven: ', obj.noConsentGiven)
+      if (obj.noConsentGiven) {
+        enabledCheckbox.setAttribute('disabled', '');
+        showConsentBtn.style.display = 'inline-block';
+      } else {
+        enabledCheckbox.removeAttribute('disabled');
+        showConsentBtn.style.display = 'none';
+      }
+    })
 
     // Connected
     let connectedColor = obj.lastSyncSuccess ? "#00AA00" : "#FF0000";
@@ -33,6 +47,11 @@ function domListeners() {
   enabled_checkbox.addEventListener("change", (obj) => {
     let enabled = obj.srcElement.checked;
     chrome.runtime.sendMessage({enabled: enabled}, function(response) {});
+  });
+  let consent_button = document.getElementById('status-consent-btn');
+  consent_button.addEventListener('click', () => {
+    const url = chrome.runtime.getURL("../static/consent.html");
+    chrome.windows.create({ url, type: "popup", height: 420, width: 416, });
   });
 }
 
